24981754fc6b16ebecb2fc105dd927fc54bd5df0
kimseo-0 <42965120+kimseo-0@users.noreply.github.com>
2022-02-19 17:21:04 +0900

    Update

--- src/App.js
+++ src/App.js
@@ -2,43 +2,14 @@ import React, { Component } from 'react';
 import {connect, Provider} from 'react-redux';
 import store from "./store";
 
-import {Button} from '@mui/material';
-import logo from './logo.svg';
 import './App.css';
-import {styled, createTheme, ThemeProvider } from '@mui/material/styles';
 
-import BodyContainer from "./containers/BodyContainer";
-
-const theme = createTheme({
-  palette: {
-    primary: {
-      main:'#1A374D',
-    },
-    secondary: {
-      main: '#406882',
-    },
-    background: {
-      main: '#FFFFFF',
-      top: '#D8D2CB',
-      back: '#EEEEEE'
-    },
-  },
-});
+import ThemeContainer from "./containers/ThemeContainer";
 
 const App = () => {
   return (
       <Provider store={store}>
-        <ThemeProvider theme={theme}>
-          <div className="App">
-            <header>
-              <meta name="viewport" content="initial-scale=1, width=device-width" />
-              <title>D.com Daily Commit</title>
-            </header>
-
-            <BodyContainer/>
-
-          </div>
-        </ThemeProvider>
+        <ThemeContainer/>
       </Provider>
   );
 }
--- src/containers/ThemeContainer.js
+++ src/containers/ThemeContainer.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import {createTheme, ThemeProvider} from "@mui/material/styles";
+import BodyContainer from "./BodyContainer";
 
 const theme = createTheme({
     palette: {
         primary: {
-            main:'#276678',
+            main:'#1A374D',
         },
         secondary: {
-            main: '#359ca5',
+            main: '#406882',
+        },
+        background: {
+            main: '#FFFFFF',
+            top: '#D8D2CB',
+            back: '#EEEEEE'
         },
     },
 });
@@ -16,7 +22,15 @@ const theme = createTheme({
 const ThemeContainer = ({}) => {
     return (
         <ThemeProvider theme={theme}>
+            <div className="App">
+                <header>
+                    <meta name="viewport" content="initial-scale=1, width=device-width" />
+                    <title>D.com Daily Commit</title>
+                </header>
 
+                <BodyContainer/>
+
+            </div>
         </ThemeProvider>
     );
 };
