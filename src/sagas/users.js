4caf968c9e6c0d289734823490b80b16f9a411a8
kimseo-0 <42965120+kimseo-0@users.noreply.github.com>
2022-02-19 19:55:10 +0900

    Update test case delete

--- src/sagas/users.js
+++ src/sagas/users.js
@@ -6,118 +6,41 @@ import axios from "axios";
 
 
 function fetchUsersAPI() {
-    // return axios.get('http://bambookim.iptime.org:8080/api/fetch-user',
-    //     {withCredentials: true});
-    const testUsers = [
-        {
-            "id": "justkode",
-            "name": "김수한무",
-            "paidFine": 0,
-            "startedAt": 1636785193000,
-            "unpaidFine": 46000,
-            "commitsInARow": 43,
-            "totalCommits": 131,
-            "participationRate": "67 / 92",
-            "rankPower": 896,
-            "rank": 1,
-            "userImg": "https://avatars.githubusercontent.com/u/28499550?v=4",
-            "commitDayCount": 67
-        },
-        {
-            "id": "codeisneverodd",
-            "name": "김경현",
-            "paidFine": 0,
-            "startedAt": 1639317384000,
-            "unpaidFine": 31500,
-            "commitsInARow": 0,
-            "totalCommits": 79,
-            "participationRate": "43 / 63",
-            "rankPower": 294,
-            "rank": 2,
-            "userImg": "https://avatars.githubusercontent.com/u/54318460?v=4",
-            "commitDayCount": 43
-        },
-    ]
-    return {data: {users : testUsers}}
+    return axios.get('/api/fetch-user',
+        {withCredentials: true});
 }
 
-function getUsersAPI() {
-    // return axios.get('http://bambookim.iptime.org:8080/api/refresh-user',
-    //     {withCredentials: true});
-    const testUsers = [
-        {
-            "id": "justkode",
-            "name": "김수한무",
-            "paidFine": 0,
-            "startedAt": 1636785193000,
-            "unpaidFine": 46000,
-            "commitsInARow": 43,
-            "totalCommits": 131,
-            "participationRate": "67 / 92",
-            "rankPower": 896,
-            "rank": 1,
-            "userImg": "https://avatars.githubusercontent.com/u/28499550?v=4",
-            "commitDayCount": 67
-        },
-        {
-            "id": "codeisneverodd",
-            "name": "김경현",
-            "paidFine": 0,
-            "startedAt": 1639317384000,
-            "unpaidFine": 31500,
-            "commitsInARow": 0,
-            "totalCommits": 79,
-            "participationRate": "43 / 63",
-            "rankPower": 294,
-            "rank": 2,
-            "userImg": "https://avatars.githubusercontent.com/u/54318460?v=4",
-            "commitDayCount": 43
-        },
-    ]
-    return {data: {users : testUsers}}
+function refreshUsersAPI() {
+    return axios.get('/api/refresh-user',
+        {withCredentials: true});
 }
 
 function addUsersAPI(data) {
-    // return axios.post('http://bambookim.iptime.org:8080/api/register-user',
-    //      {
-    //         githubId: {date.githubId}
-    //         korName: {data.korName}
-    //      },
-    //     {withCredentials: true});
-    const testUser =
-        {
-            "id": "kimseo-0",
-            "name": "김서영",
-            "paidFine": 0,
-            "startedAt": 1636785193000,
-            "unpaidFine": 46000,
-            "commitsInARow": 43,
-            "totalCommits": 131,
-            "participationRate": "67 / 92",
-            "rankPower": 896,
-            "rank": 4,
-            "userImg": "https://avatars.githubusercontent.com/u/28499550?v=4",
-            "commitDayCount": 67
-        }
-
-    return {data: {user : testUser}}
+    return axios.post('/api/register-user', null,
+         {
+            params: {
+                githubId: data.githubId,
+                korName: data.korName
+            },
+             withCredentials: true
+         });
 }
 
 function deleteUsersAPI(data) {
-    // return axios.post('http://bambookim.iptime.org:8080/api/fetch-user',
-    //      {
-    //         githubId: {data.githubId}
-    //      },
-    //     {withCredentials: true});
-    return {data: {githubId : 'justkode'}}
+    return axios.post('/api/delete-user', null,
+         {
+            params: {githubId: data.githubId},
+             withCredentials: true
+         },
+        );
 }
 
 
 function* fetchUsers(action) {
     try {
         const res = yield call(fetchUsersAPI);
-        const {users} = res.data;
-        console.log(users)
+        const users = res.data;
+        console.log(res.data)
         yield put({type: UPDATE_USERS, data: {users : users}})
     } catch (e) {
         console.error(e);
@@ -125,10 +48,10 @@ function* fetchUsers(action) {
     }
 }
 
-function* getUsers(action) {
+function* refreshUsers(action) {
     try {
-        const res = yield call(getUsersAPI);
-        const {users} = res.data;
+        const res = yield call(refreshUsersAPI);
+        const users = res.data;
         console.log(users)
         yield put({type: UPDATE_USERS, data: {users : users}})
     } catch (e) {
@@ -141,7 +64,7 @@ function* addUser(action) {
     try {
         const data = action.data;
         const res = yield call(addUsersAPI, data);
-        const {user} = res.data;
+        const user = res.data;
         yield put({type: UPDATE_ADD_USER, data: {user : user}})
     } catch (e) {
         console.error(e);
@@ -151,8 +74,10 @@ function* addUser(action) {
 function* deleteUser(action) {
     try {
         const data = action.data;
+        console.log(data)
         const res = yield call(deleteUsersAPI, data);
-        const {githubId} = res.data;
+        console.log(res)
+        const githubId = res.data;
         yield put({type: UPDATE_DELETE_USER, data: {githubId : githubId}})
     } catch (e) {
         console.error(e);
@@ -164,8 +89,8 @@ function* fetchUsersWatch() {
     yield takeEvery("FETCH_USERS", fetchUsers);
 }
 
-function* getUsersWatch() {
-    yield takeEvery("REFRESH_USERS", getUsers);
+function* refreshUsersWatch() {
+    yield takeEvery("REFRESH_USERS", refreshUsers);
 }
 
 function* addUserWatch() {
@@ -180,7 +105,7 @@ function* deleteUserWatch() {
 export default function* userSaga() {
     yield all([
         fork(fetchUsersWatch),
-        fork(getUsersWatch),
+        fork(refreshUsersWatch),
         fork(addUserWatch),
         fork(deleteUserWatch)
     ])
