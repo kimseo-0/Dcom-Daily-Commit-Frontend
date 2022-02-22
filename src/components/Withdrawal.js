a329cafac7079a422c7a182cf64f21cf9a518789
kimseo-0 <42965120+kimseo-0@users.noreply.github.com>
2022-02-22 18:06:48 +0900

    Exception handling

--- src/components/Withdrawal.js
+++ src/components/Withdrawal.js
@@ -1,27 +1,60 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {
     Dialog, DialogTitle, DialogContent,
     Button, IconButton, Typography, Alert, Box, TextField, CircularProgress
 } from '@mui/material';
 import CloseIcon from '@mui/icons-material/Close';
 
-const Withdrawal = ({open, handleClose, deleteUser, deleteUserLoading}) => {
+const Withdrawal = ({info, open, handleClose, deleteUser, deleteUserLoading}) => {
     const [account, setAccount] = useState({
         githubId: "",
         userCode: ""
     });
+    const [submitValid, setSubmitValid] = useState(false);
+    const [userCodeValid, setUserCodeValid] = useState(false);
+    const [openInfo, setOpenInfo] = React.useState(false);
+
+    useEffect(() => {
+        if (info.type !== null) {
+            setOpenInfo(true)
+        }
+    },[info]);
 
     const onChangeAccount = (e) => {
-        setAccount({
-            ...account,
-            [e.target.id]: e.target.value,
-        });
+        if (e.target.id === 'userCode') {
+            if ( e.target.value.length === 4 ) {
+                setAccount({
+                    ...account,
+                    [e.target.id]: e.target.value,
+                });
+                setUserCodeValid(true);
+            }
+            else if (e.target.value.length < 4) {
+                setAccount({
+                    ...account,
+                    [e.target.id]: e.target.value.replace(/[^0-9]/g, ""),
+                });
+                setUserCodeValid(false);
+            }
+            else {
+                setUserCodeValid(true);
+            }
+        } else {
+            setAccount({
+                ...account,
+                [e.target.id]: e.target.value,
+            });
+        }
+        if (account.githubId !==''&& account.userCode !== '') {
+            setSubmitValid(true)
+        } else {
+            setSubmitValid(true)
+        }
     };
 
     const submitForm = () => {
-        if (account.githubId !== "" && account.userCode !== "" ) {
+        if (submitValid) {
             deleteUser(account);
-            handleClose();
         }
     }
 
@@ -40,6 +73,13 @@ const Withdrawal = ({open, handleClose, deleteUser, deleteUserLoading}) => {
                 <DialogContent dividers>
                     <Box component="form" noValidate autoComplete="off" maxWidth="sm"
                         sx={{ margin: 4, marginTop: 2, width: {sm : 450}}} >
+                        {   openInfo ?
+                            <Alert severity="error" sx={{fontFamily: "Anton", marginTop: 2}} >
+                                {info.message}
+                            </Alert>
+                            :
+                            ""
+                        }
                         <Alert severity="error" sx={{fontFamily: "Anton", marginBottom:2}}>
                             사용자 제거 시 모든 정보가 삭제되며 복구할 수 없습니다.
                         </Alert>
@@ -47,11 +87,13 @@ const Withdrawal = ({open, handleClose, deleteUser, deleteUserLoading}) => {
                         <Box sx={{marginBottom:4}}>
                             <TextField required id="githubId" label="Github Id" onChange={onChangeAccount}
                                        variant="standard" fullWidth margin={"dense"} color='secondary' />
-                            <TextField required id="userCode" label="등록 시 사용한 User Code" onChange={onChangeAccount}
-                                       variant="standard" fullWidth margin={"dense"} />
+                            <TextField required id="userCode" label="User Code" onChange={onChangeAccount} value={account.userCode}
+                                       variant="standard" fullWidth margin={"dense"} color='secondary' error={!userCodeValid && account.userCode.length !== 0}
+                                       helperText="4자리 숫자를 입력해주세요."/>
                         </Box>
 
-                        <Button onClick={submitForm} variant="contained" fullWidth color='error' sx={{fontFamily:"NanumGothicExtraBold"}}>
+                        <Button onClick={submitForm} variant="contained" fullWidth color='error' disabled={deleteUserLoading || (!submitValid || !userCodeValid)}
+                                sx={{fontFamily:"NanumGothicExtraBold"}}>
                             {deleteUserLoading ?
                                 <CircularProgress color='inherit'/>
                                 :
