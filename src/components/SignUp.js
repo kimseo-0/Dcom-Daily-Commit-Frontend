73290a247ffb428d8f7ae912438a169608c31dc5
kimseo-0 <42965120+kimseo-0@users.noreply.github.com>
2022-02-28 16:18:01 +0900

    Update sign up / withdrawal
    add valid condition
    add github helper text
    move 'sign up error info' top to bottom

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -10,31 +10,67 @@ const SignUp = ({info, open, handleClose, addUser, addUserLoading}) => {
         korName: "",
     });
     const [submitValid, setSubmitValid] = useState(false);
+    const [githubIdValid, setGithubIdValid] = useState(true);
     const [korNameValid, setKorNameValid] = useState(true);
+    const [accessCodeValid, setAccessCodeValid] = useState(true);
     const [userCodeValid, setUserCodeValid] = useState(true);
     const [openInfo, setOpenInfo] = React.useState(false);
 
     useEffect(() => {
         if (info.type !== null) {
-            setOpenInfo(true)
+            setOpenInfo(true);
+            setSubmitValid(false);
         } else {
             clearForm();
         }
     },[info]);
 
     const onChangeAccount = (e) => {
+        setSubmitValid(true);
         const kor_regex = /[^(ㄱ-ㅎ|ㅏ-ㅣ|가-힣)]/;
         const num_regex = /[^0-9]/;
-        if (e.target.id === 'korName') {
-            if (kor_regex.test(e.target.value)) {
-                setKorNameValid(false);
-            }
-            else if (e.target.value.length === 0) {
+        if (e.target.id === 'githubId') {
+            if (e.target.value === "") {
                 setAccount({
                     ...account,
                     [e.target.id]: e.target.value,
                 });
-                setKorNameValid(true);
+                setGithubIdValid(false);
+            }
+            else {
+                setAccount({
+                    ...account,
+                    [e.target.id]: e.target.value,
+                });
+                setGithubIdValid(true);
+            }
+        }
+        if (e.target.id === 'accessCode') {
+            if (e.target.value === "") {
+                setAccount({
+                    ...account,
+                    [e.target.id]: e.target.value,
+                });
+                setAccessCodeValid(false);
+            }
+            else {
+                setAccount({
+                    ...account,
+                    [e.target.id]: e.target.value,
+                });
+                setAccessCodeValid(true);
+            }
+        }
+        else if (e.target.id === 'korName') {
+            if (e.target.value === "") {
+                setAccount({
+                    ...account,
+                    [e.target.id]: e.target.value,
+                });
+                setKorNameValid(false);
+            }
+            else if (kor_regex.test(e.target.value)) {
+                setKorNameValid(false);
             }
             else if ( e.target.value.length >= 1 && e.target.value.length <= 5 ) {
                 setAccount({
@@ -48,15 +84,15 @@ const SignUp = ({info, open, handleClose, addUser, addUserLoading}) => {
             }
         }
         else if (e.target.id === 'userCode') {
-            if (num_regex.test(e.target.value)) {
-                setUserCodeValid(false);
-            }
-            else if (e.target.value.length === 0) {
+            if (e.target.value === "") {
                 setAccount({
                     ...account,
                     [e.target.id]: e.target.value,
                 });
-                setUserCodeValid(true);
+                setUserCodeValid(false);
+            }
+            else if (num_regex.test(e.target.value)) {
+                setUserCodeValid(false);
             }
             else if ( e.target.value.length === 4 ) {
                 setAccount({
@@ -72,9 +108,6 @@ const SignUp = ({info, open, handleClose, addUser, addUserLoading}) => {
                 });
                 setUserCodeValid(false);
             }
-            else {
-                setUserCodeValid(false);
-            }
         }
         else {
             setAccount({
@@ -85,7 +118,8 @@ const SignUp = ({info, open, handleClose, addUser, addUserLoading}) => {
 
         if (account.githubId !=='' && account.korName !== '' && account.userCode !== '' && account.accessCode !== '') {
             setSubmitValid(true)
-        } else {
+        }
+        else {
             setSubmitValid(false)
         }
     };
@@ -103,6 +137,12 @@ const SignUp = ({info, open, handleClose, addUser, addUserLoading}) => {
             userCode: "",
             korName: "",
         })
+        setSubmitValid(false);
+        setGithubIdValid(true);
+        setKorNameValid(true);
+        setUserCodeValid(true);
+        setAccessCodeValid(true);
+        setOpenInfo(false);
     }
 
     return (
@@ -112,7 +152,7 @@ const SignUp = ({info, open, handleClose, addUser, addUserLoading}) => {
                         sx={{fontFamily: "Anton", textAlign:'center'}} >
                         Sign Up
                     </Typography>
-                    <IconButton onClick={() => {handleClose(); clearForm(); }}
+                    <IconButton onClick={() => {handleClose(); clearForm(); }} disabled={addUserLoading}
                         sx={{ position: 'absolute', right: 8, top: 8,
                             color: 'close' }} >
                         <CloseIcon />
@@ -122,16 +162,10 @@ const SignUp = ({info, open, handleClose, addUser, addUserLoading}) => {
                     <Box
                         component="form" noValidate autoComplete="off" maxWidth="sm"
                         sx={{ margin: 4, marginTop: 0, marginBottom: 2, width: {sm : 450}}} >
-                        {   openInfo && (info.type !== null) ?
-                            <Alert severity="error" variant='filled' sx={{fontFamily: "Anton", marginTop: 2}} >
-                                {info.message}
-                            </Alert>
-                            :
-                            null
-                        }
                         <Box sx={{marginTop: 1, marginBottom: 1}}>
-                            <TextField required id="githubId" label="GitHub ID" onChange={onChangeAccount} focused={info.focus === "githubId"} error={info.focus === "githubId"}
-                                       variant="standard" fullWidth margin={"dense"} color='secondary' />
+                            <TextField required id="githubId" label="GitHub ID" onChange={onChangeAccount} focused={info.focus === "githubId"}
+                                       variant="standard" fullWidth margin={"dense"} color='secondary' sx={{whiteSpace: 'pre-wrap'}} error={!githubIdValid || info.focus === "korName"}
+                                       helperText={"본인의 github 프로필 URL 속 ID를 입력해주세요. \n ex) URL : github.com/kimseo-0 → ID : kiseo-0"} />
                             <TextField required id="korName" label="Name" onChange={onChangeAccount} value={account.korName} focused={info.focus === "korName"}
                                        variant="standard" fullWidth margin={"dense"} color='secondary' error={!korNameValid || info.focus === "korName"}
                                        helperText="한글 실명(1- 5자리)을 입력해주세요. ex) 홍길동" />
@@ -140,7 +174,7 @@ const SignUp = ({info, open, handleClose, addUser, addUserLoading}) => {
                             <Alert severity="info" sx={{fontFamily: "Anton"}}>
                                 Access Code는 단톡방에서 확인할 수 있습니다.
                             </Alert>
-                            <TextField required id="accessCode" label="Access Code" onChange={onChangeAccount} focused={info.focus === "accessCode"} error={info.focus === "accessCode"}
+                            <TextField required id="accessCode" label="Access Code" onChange={onChangeAccount} focused={info.focus === "accessCode"} error={!accessCodeValid || info.focus === "korName"}
                                        variant="standard" fullWidth margin={"dense"} color='secondary'/>
                         </Box>
                         <Box sx={{marginTop: 1, marginBottom: 2}}>
@@ -148,13 +182,22 @@ const SignUp = ({info, open, handleClose, addUser, addUserLoading}) => {
                                 User Code는 사용자 삭제시 사용됩니다.<br/>
                                 User Code는 암호화 되지 않고 관리자가 볼 수 있습니다.
                             </Alert>
-                            <TextField required id="userCode" label="User Code" onChange={onChangeAccount} value={account.userCode} focused={info.focus === "userCode"}
+                            <TextField type="password" required id="userCode" label="User Code" onChange={onChangeAccount} value={account.userCode} focused={info.focus === "userCode"}
                                        variant="standard" fullWidth margin={"dense"} color='secondary' error={!userCodeValid || info.focus === "userCode"}
                                        helperText="4자리 숫자를 입력해주세요. ex) 1234"/>
                         </Box>
 
-                        <Button onClick={submitForm} variant="contained" fullWidth disabled={addUserLoading || (!submitValid || !userCodeValid)}
-                                color="success" sx={{fontFamily:"NanumGothicExtraBold"}}>
+
+                        {   openInfo && (info.type !== null) ?
+                            <Alert id="signUpInfo" severity="error" variant='filled' sx={{fontFamily: "Anton", marginTop: 2}} >
+                                {info.message}
+                            </Alert>
+                            :
+                            null
+                        }
+
+                        <Button id="signUpButton" onClick={submitForm} variant="contained" fullWidth disabled={addUserLoading || (!submitValid || !userCodeValid || !korNameValid || !accessCodeValid || !githubIdValid)}
+                                color="success" sx={{fontFamily:"NanumGothicExtraBold", marginTop: 2}}>
                                 {addUserLoading ?
                                     <CircularProgress color='inherit'/>
                                 :
--- src/components/Withdrawal.js
+++ src/components/Withdrawal.js
@@ -12,6 +12,7 @@ const Withdrawal = ({info, open, handleClose, deleteUser, deleteUserLoading}) =>
     });
     const [checked, setChecked] = useState(false);
     const [submitValid, setSubmitValid] = useState(false);
+    const [githubIdValid, setGithubIdValid] = useState(true);
     const [userCodeValid, setUserCodeValid] = useState(true);
     const [openInfo, setOpenInfo] = useState(false);
 
@@ -25,16 +26,32 @@ const Withdrawal = ({info, open, handleClose, deleteUser, deleteUserLoading}) =>
 
     const onChangeAccount = (e) => {
         const num_regex = /[^0-9]/;
-        if (e.target.id === 'userCode') {
-            if (num_regex.test(e.target.value)) {
-                setUserCodeValid(false);
-            }
-            else if (e.target.value.length === 0) {
+        if (e.target.id === 'githubId') {
+            if (e.target.value === "") {
                 setAccount({
                     ...account,
                     [e.target.id]: e.target.value,
                 });
-                setUserCodeValid(true);
+                setGithubIdValid(false);
+            }
+            else {
+                setAccount({
+                    ...account,
+                    [e.target.id]: e.target.value,
+                });
+                setGithubIdValid(true);
+            }
+        }
+        else if (e.target.id === 'userCode') {
+            if (e.target.value === "") {
+                setAccount({
+                    ...account,
+                    [e.target.id]: e.target.value,
+                });
+                setUserCodeValid(false);
+            }
+            else if (num_regex.test(e.target.value)) {
+                setUserCodeValid(false);
             }
             else if ( e.target.value.length === 4 ) {
                 setAccount({
@@ -50,9 +67,6 @@ const Withdrawal = ({info, open, handleClose, deleteUser, deleteUserLoading}) =>
                 });
                 setUserCodeValid(false);
             }
-            else {
-                setUserCodeValid(false);
-            }
         }
         else {
             setAccount({
@@ -63,7 +77,8 @@ const Withdrawal = ({info, open, handleClose, deleteUser, deleteUserLoading}) =>
 
         if (account.githubId !==''&& account.userCode !== '') {
             setSubmitValid(true)
-        } else {
+        }
+        else {
             setSubmitValid(true)
         }
     };
@@ -84,49 +99,50 @@ const Withdrawal = ({info, open, handleClose, deleteUser, deleteUserLoading}) =>
             userCode: ""
         })
         setChecked(false);
+        setSubmitValid(false);
+        setGithubIdValid(true);
+        setUserCodeValid(true);
+        setOpenInfo(false);
     }
 
     return (
             <Dialog onClose={() => {handleClose(); clearForm(); }} open={open} >
                 <DialogTitle sx={{ m: 0, p: 2}}>
-                    <Typography variant="h5" noWrap component="div" color="error"
-                        sx={{fontFamily: "Anton", textAlign:'center'}} >
+                    <Typography variant="h5" noWrap component="div" color="error" sx={{fontFamily: "Anton", textAlign:'center'}} >
                         User Delete
                     </Typography>
-                    <IconButton onClick={() => {handleClose(); clearForm(); }}
-                        sx={{ position: 'absolute', right: 8, top: 8, color: 'close'}} >
+                    <IconButton onClick={() => {handleClose(); clearForm(); }} disabled={deleteUserLoading} sx={{ position: 'absolute', right: 8, top: 8, color: 'close'}} >
                         <CloseIcon />
                     </IconButton>
                 </DialogTitle>
                 <DialogContent dividers>
                     <Box component="form" noValidate autoComplete="off" maxWidth="sm"
                         sx={{ margin: 4, marginTop: 2, width: {sm : 450}}} >
+
                         {   openInfo && (info.type !== null) ?
-                            <Alert severity="error" variant='filled' sx={{fontFamily: "Anton", marginTop: 2}} >
+                            <Alert severity="error" variant='outlined' sx={{fontFamily: "Anton", marginTop: 2}} >
                                 {info.message}
                             </Alert>
                             :
                             null
                         }
+
                         <Box sx={{marginBottom:4}}>
-                            <TextField required id="githubId" label="GitHub ID" onChange={onChangeAccount} focused={info.focus === "githubId"} error={info.focus === "githubId"}
-                                       variant="standard" fullWidth margin={"dense"} color='secondary' />
-                            <TextField required id="userCode" label="User Code" onChange={onChangeAccount} value={account.userCode} focused={info.focus === "userCode"}
+                            <TextField required id="githubId" label="GitHub ID" onChange={onChangeAccount} focused={info.focus === "githubId"} error={!githubIdValid || info.focus === "githubId"}
+                                       variant="standard" fullWidth margin={"dense"} color='secondary' sx={{whiteSpace: 'pre-wrap'}}
+                                       helperText={"본인의 github 프로필 URL 속 ID를 입력해주세요. \n ex) URL : github.com/kimseo-0 → ID : kiseo-0"} />
+                            <TextField type="password" required id="userCode" label="User Code" onChange={onChangeAccount} value={account.userCode} focused={info.focus === "userCode"}
                                        variant="standard" fullWidth margin={"dense"} color='secondary' error={!userCodeValid || info.focus === "userCode"}
-                                       helperText="4자리 숫자를 입력해주세요."/>
+                                       helperText="4자리 숫자를 입력해주세요. ex) 1234"/>
                         </Box>
 
-                        <Alert severity="error" sx={{fontFamily: "Anton", marginBottom:2}}
+                        <Alert severity="error" variant={checked ? "filled" : "standard"} sx={{fontFamily: "Anton", marginBottom:2, whiteSpace: "pre-wrap"}}
                                action={
-                                   <Checkbox
-                                       color='error'
-                                       checked={checked}
-                                       onChange={onChangeChecked}
-                                       inputProps={{ 'aria-label': 'controlled' }}
-                                   />
+                                   <Checkbox color='primary' inputProps={{ 'aria-label': 'controlled' }}
+                                             checked={checked} onChange={onChangeChecked} />
                                }
                         >
-                            사용자 제거 시 모든 정보가 삭제되며 복구할 수 없습니다.
+                            {"사용자 제거 시 모든 정보가 삭제되며 복구할 수 없습니다."}
                         </Alert>
 
                         <Button onClick={submitForm} variant="contained" fullWidth color='error' disabled={deleteUserLoading || (!submitValid || !userCodeValid) || !checked}
