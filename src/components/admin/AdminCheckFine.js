a8f2c1eb498953f20dca2bb767908848cd8ffd56
kimseo-0 <42965120+kimseo-0@users.noreply.github.com>
2022-04-04 00:07:00 +0900

    Update clear code

--- src/components/admin/AdminCheckFine.js
+++ src/components/admin/AdminCheckFine.js
@@ -1,41 +1,35 @@
 import React, {Component, useState} from 'react';
-import {
-    Alert,
-    Avatar, Box,
-    Button,
-    IconButton,
-    Paper,
-    Table,
-    TableBody,
-    TableCell,
-    TableContainer,
-    TableHead,
-    TableRow,
-    TextField,
-    Typography
-} from "@mui/material";
+import { Button, IconButton, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField } from "@mui/material";
 import logo from "../../logo192.png";
+import PriceCheckIcon from '@mui/icons-material/PriceCheck';
 
 const LoginAdmin = ({users}) => {
     const tableRendering = () => {
         const result = [];
         for (let i = 0; i < users.length; i++) {
             let user = users[i]
-            result.push(<TableRow key={user.name}>
-                <TableCell component="th" scope="row" align='center' sx={{fontFamily:"NanumGothicRegular"}}>
-                    {i + 1}
-                </TableCell>
-                <TableCell align="left" sx={{fontFamily:"NanumGothicRegular"}}>
-                    {user.name}
-                </TableCell>
-                <TableCell align="center" sx={{fontFamily:"NanumGothicRegular"}}>
-                    {user.unpaidFine}원
-                </TableCell>
-                <TableCell align="center" sx={{fontFamily:"NanumGothicRegular"}}>{user.paidFine}원</TableCell>
-                <TableCell align="center" sx={{fontFamily:"NanumGothicRegular"}}>
-
-                </TableCell>
-            </TableRow>)
+            if (user.paidFine > 0) {
+                result.push(<TableRow key={user.name}>
+                    <TableCell component="th" scope="row" align='center' sx={{fontFamily:"NanumGothicRegular"}}>
+                        {i + 1}
+                    </TableCell>
+                    <TableCell align="center" sx={{fontFamily:"NanumGothicRegular"}}>
+                        {user.name}
+                    </TableCell>
+                    <TableCell align="center" sx={{fontFamily:"NanumGothicRegular"}}>
+                        {user.paidFine}원
+                    </TableCell>
+                    <TableCell align="center" sx={{fontFamily:"NanumGothicRegular"}}>
+                        {user.unpaidFine}원
+                    </TableCell>
+                    <TableCell align="right" sx={{fontFamily:"NanumGothicRegular", width: "150px"}}>
+                        <TextField id="paidedFine" label="납부 금액" color="secondary" variant="outlined" size="small" sx={{width: "100px"}}/>
+                        <IconButton color="point" sx={{marginLeft: 1}}>
+                            <PriceCheckIcon/>
+                        </IconButton>
+                    </TableCell>
+                </TableRow>)
+            }
         }
         return result;
     }
@@ -43,13 +37,13 @@ const LoginAdmin = ({users}) => {
     return (
         <div>
             <TableContainer component={Paper} sx={{clear: "both"}}>
-                <Table sx={{ minWidth: 600 }} size="small">
+                <Table sx={{ minWidth: 600 }}>
                     <TableHead>
                         <TableRow>
                             <TableCell sx={{fontFamily:"NanumGothicExtraBold"}} align='center'>랭킹</TableCell>
-                            <TableCell sx={{fontFamily:"NanumGothicExtraBold", width: 130}} align='center'>프로필</TableCell>
-                            <TableCell sx={{fontFamily:"NanumGothicExtraBold"}} align='center'>남은 벌금</TableCell>
+                            <TableCell sx={{fontFamily:"NanumGothicExtraBold"}} align='center'>프로필</TableCell>
                             <TableCell sx={{fontFamily:"NanumGothicExtraBold"}} align='center'>낸 벌금</TableCell>
+                            <TableCell sx={{fontFamily:"NanumGothicExtraBold"}} align='center'>남은 벌금</TableCell>
                             <TableCell sx={{fontFamily:"NanumGothicExtraBold"}} align='center'/>
                         </TableRow>
                     </TableHead>
